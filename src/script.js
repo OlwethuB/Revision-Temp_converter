6f7f3402f017c4bbdab20542c2decd55c544f805
OlwethuB <olwethu.a.bhiya@gmail.com>
2023-05-29 23:51:29 +0200

    edited

--- src/script.js
+++ src/script.js
@@ -1,13 +1,13 @@
 // Function to convert Celsius to Fahrenheit
 function celsiusToFahrenheit(celsius) {
   // TODO: Implement the conversion logic here
-  output = (temperature * (9 / 5) + 32);
+  tofahrenheit = (temperature * (9 / 5) + 32);
 }
 
 // Function to convert Fahrenheit to Celsius
 function fahrenheitToCelsius(fahrenheit) {
   // TODO: Implement the conversion logic here\
-  output = (temperature - 32) * 5/9;
+  tocelsius = (temperature - 32) * 5/9;
 }
 
 // Prompt the user for input
@@ -19,11 +19,13 @@ let conversionType = prompt(
 if (conversionType.toLowerCase() === "celsius to fahrenheit") {
   // TODO: Call the celsiusToFahrenheit function and display the result
   celsiusToFahrenheit();
-  console.log
+    document.getElementById("result").innerHTML = celsiusToFahrenheit(celsius, tofahrenheit) + "&#176; Fahrenheit";
+    
 } else if (conversionType.toLowerCase() === "fahrenheit to celsius") {
   // TODO: Call the fahrenheitToCelsius function and display the result
   fahrenheitToCelsius();
-  console.log 
+    document.getElementById("result").innerHTML = fahrenheitToCelsius(fahrenheit, tocelsius) + "&#176; Celsius";
+    
 } else {
   alert("Invalid conversion type.");
 }
